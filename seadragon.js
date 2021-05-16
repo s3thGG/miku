fa76a0b68d069ca5a3806bfb580b5dac90d39309
sethGG <76197695+s3thGG@users.noreply.github.com>
2021-05-15 21:21:00 -0700

    Update seadragon.js

--- seadragon.js
+++ seadragon.js
@@ -4,9 +4,9 @@
 // License: MIT
 // Edit: s3thGG
 (function() {
-  var ASSET = "https://cdn.jsdelivr.net/gh/s3thGG/TEST-REPO--BY-s3thGG-/dvdicon.png";
-  var WIDTH = 256;
-  var HEIGHT = 256;
+  var ASSET = "https://cdn.jsdelivr.net/gh/s3thGG/miku/hat1.gif";
+  var WIDTH = 300;
+  var HEIGHT = 289;
   var VERT_DIRECTIONS = ["up", "down"];
   var HORI_DIRECTIONS = ["left", "right"];
   var SCROLL_AMOUNTS = [6, 8, 11, 15];
